09e9762a1d74d5bf9e71cabd2adee478775b3935
ebinezer.p87@gmail.com <ebinezer.p87@gmail.com>
2020-01-03 14:54:58 +0530

    first push

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  elementType: 'url' | 'canvas' | 'img' = 'img';
+  value: string;
+  batchCode: string;
+  date: string = new Date().getDate().toString();
+  time: string = new Date().getTime().toString();
   constructor() {}
 
+  generate() {
+    this.value = (this.batchCode + this.date + this.time);
+  }
 }
